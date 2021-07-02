319339e9daa3c61365a6bb7341597e4004b0f0cf
Sujon Al Mahmud <sujon.almahmud@welldev.io>
2021-07-02 19:21:11 +0600

    add method to render Message to the DOM

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,3 +1,5 @@
+import { createElement as h, toDateTime } from "../js/helpers";
+
 export default class Message {
   author;
   message;
@@ -9,5 +11,17 @@ export default class Message {
     this.author = author
     this.message = message
     this.timestamp = timestamp
+  }
+
+  render(incoming = true) {
+    return h('li', [
+      h('div', [
+        incoming && h('span', [this.author]),
+        h('p', [this.message]),
+        h('span', [
+            toDateTime(this.timestamp)
+        ], {class: 'timestamp'})
+      ].filter(Boolean), {class: 'message'})
+    ], {class: !incoming ? 'incoming' : 'outgoing'})
   }
 }
