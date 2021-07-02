6668a796ab2730d353e0ac3190d8b11b8002b142
Sujon Al Mahmud <sujon.almahmud@welldev.io>
2021-07-02 19:31:46 +0600

    scroll to view the last message

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,14 @@ import ChatService from "../service/ChatService";
 const { chat } = document.forms
 const [$message] = chat.elements
 const $messages = document.querySelector('#messages')
+const $viewport = document.querySelector('.chat--window-viewport')
+const EVENT_VIEW = 'view'
+
+$viewport.addEventListener(EVENT_VIEW, (evt) => {
+  const {scrollHeight} = evt.target
+
+  $viewport.scrollTo({behavior: "smooth", top: scrollHeight})
+})
 
 const service = new ChatService(
     process.env.API_KEY,
@@ -19,6 +27,8 @@ chat.addEventListener('submit', async evt => {
 
     $message.value = null
     $messages.appendChild(msg.render(false))
+    $viewport.dispatchEvent(new CustomEvent(EVENT_VIEW))
+    $message.focus()
   } catch (err) {
     console.error(err)
   }
@@ -32,6 +42,12 @@ const SENDER = process.env.API_USER
 
     messages.map(msg => msg.render(msg.author !== SENDER))
         .forEach(n => $messages.appendChild(n))
+
+    if (!messages.length) {
+      return
+    }
+
+    $viewport.dispatchEvent(new CustomEvent(EVENT_VIEW))
   } catch (err) {
     console.error(err)
   }
